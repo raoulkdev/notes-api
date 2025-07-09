4ff307d4f47055b6754e7233b4b5ca1f0eef7e5e
Raoul Kaleba <raouldev@outlook.com>
2025-07-09 12:33:54 -0500

    Setup server starting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+// Module Declarations
 mod server;
-mod note;
+mod notes;
 
-fn main() {
-    println!("Hello, world!");
+// Imports
+use crate::server::Server;
+
+#[tokio::main]
+async fn main() {
+    let server = Server::new("0.0.0.0:3000");
+    server.start().await;
 }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,10 @@
-use std::sync::{Arc, Mutex};
 // Imports
-use crate::note::Note;
+use std::sync::{Arc, Mutex};
+use axum::Router;
+use axum::routing::get;
+use colored::Colorize;
+use tokio::net::TcpListener;
+use crate::notes::Note;
 
 pub struct Server {
     address: & 'static str,
@@ -8,7 +12,25 @@ pub struct Server {
 }
 
 impl Server {
+    // Create a new server instance
     pub fn new(address: & 'static str) -> Self {
         Self {address, notes: Arc::new(Mutex::new(vec![]))}
+    }
+    
+    // Start server
+    pub async fn start(&self) {
+        // Bind TCP listener & check if successfull
+        let listener = TcpListener::bind(self.address).await;
+        match listener { 
+            Ok(addr) => {
+                println!("{}{}", "Server successfully started on: ".bright_green(), addr.local_addr().unwrap());
+                axum::serve(addr, Self::router()).await.unwrap();
+            }
+            Err(e) => println!("{}{e}", "Could not start server: ".bright_red())
+        }
+    }
+    
+    fn router() -> Router {
+        Router::new().route("/", get("Hello World"))
     }
 }

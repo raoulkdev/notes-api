b37dcd9fc82401c3b45b484e8d47fcffa8f076be
Raoul Kaleba <raouldev@outlook.com>
2025-07-09 15:29:15 -0500

    cleaned up and fixed structure

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use crate::server::Server;
 
 #[tokio::main]
 async fn main() {
+    // Create a new server
     let server = Server::new("0.0.0.0:3000");
+
+    // Start server
     server.start().await;
 }
